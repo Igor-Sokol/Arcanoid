b80b0876baba640404d5d333eb04b8f67f981536
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-01 17:46:03 +0300

    Added BlockContainer, in the future its task will be to store the currently available blocks in the scene

---
+++ Assets/Application/Scripts/Application/Scenes/Game/PlayingField/PlayingBlockContainer/PlayingBlockContainer.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Application.Scripts.Application.Scenes.Game.PlayingField.PlayingBlockContainer
+{
+    public class PlayingBlockContainer : MonoBehaviour
+    {
+        
+    }
+}
