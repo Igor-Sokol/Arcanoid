d4706ec94fb6e11b8ef674c89ad543ff508f1bbd
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-03 13:53:38 +0300

    Odin Inspector installed

--- Assets/Application/Scripts/Library/SceneManagers/Contracts/SceneInfo/SceneInfo.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Application.Scripts.Library.SceneManagers.Contracts.SceneInfo
-{
-    public abstract class SceneInfo : MonoBehaviour
-    {
-        public abstract string SceneName { get; }
-    }
-}
