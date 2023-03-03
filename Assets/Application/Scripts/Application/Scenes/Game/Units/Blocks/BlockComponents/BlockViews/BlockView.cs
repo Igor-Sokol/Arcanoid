c5ce4163e1af36ccc06a805214a549e45824a4b9
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-03 14:03:52 +0300

    BlockView.GetWordSize replaced to Image.sprite.bounds.size

--- Assets/Application/Scripts/Application/Scenes/Game/Units/Blocks/BlockComponents/BlockViews/BlockView.cs
+++ Assets/Application/Scripts/Application/Scenes/Game/Units/Blocks/BlockComponents/BlockViews/BlockView.cs
@@ -5,25 +5,7 @@ namespace Application.Scripts.Application.Scenes.Game.Units.Blocks.BlockComponen
     public class BlockView : MonoBehaviour
     {
         [SerializeField] private SpriteRenderer spriteRenderer;
-        
-        public Vector2 GetWordSize()
-        {
-            Sprite sprite = spriteRenderer.sprite;
-            Vector2 pixels = new Vector2(sprite.rect.width, sprite.rect.height);
 
-            float pixelPerUnit = sprite.pixelsPerUnit;
-
-            return new Vector2(pixels.x / pixelPerUnit, pixels.y / pixelPerUnit);
-        }
-
-        public void SetWordSize(Vector2 size)
-        {
-            Sprite sprite = spriteRenderer.sprite;
-            float pixelPerUnit = sprite.pixelsPerUnit;
-            Rect imageRect = sprite.rect;
-
-            transform.localScale = new Vector3(imageRect.width / pixelPerUnit / size.x,
-                imageRect.height / pixelPerUnit / size.y);
-        }
+        public Sprite Sprite => spriteRenderer.sprite;
     }
 }
