a43d165212032e9155049e27e713dff1ab5c89ea
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-04 20:32:03 +0300

    TimeManger changed to run without serialization

--- Assets/Application/Scripts/Library/TimeManagers/TimeManager.cs
+++ Assets/Application/Scripts/Library/TimeManagers/TimeManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Application.Scripts.Library.TimeManagers.Contracts;
 using UnityEngine;
 
@@ -5,11 +6,22 @@ namespace Application.Scripts.Library.TimeManagers
 {
     public class TimeManager : MonoBehaviour
     {
-        [SerializeField] private TimeScaler[] timeScales;
+        private List<ITimeScaler> _timeScales;
 
+        public IEnumerable<ITimeScaler> TimeScales => _timeScales; 
         public float DeltaTime => GetDeltaTime();
         public float Scale => GetScale();
 
+        public void AddTimeScaler(ITimeScaler scaler)
+        {
+            _timeScales.Add(scaler);
+        }
+
+        public void RemoveTimeScaler(ITimeScaler scaler)
+        {
+            _timeScales.Remove(scaler);
+        }
+        
         private float GetDeltaTime()
         {
             return Time.deltaTime * GetScale();
@@ -19,7 +31,7 @@ namespace Application.Scripts.Library.TimeManagers
         {
             float scale = 1f;
 
-            foreach (var scaler in timeScales)
+            foreach (var scaler in _timeScales)
             {
                 scale *= scaler.Scale;
             }
