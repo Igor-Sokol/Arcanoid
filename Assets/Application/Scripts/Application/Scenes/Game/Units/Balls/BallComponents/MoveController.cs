0d7c9437a0939e204f2b9f4ac3f8566a47977797
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-04 20:38:24 +0300

    For the ball, a component has been added to control its bounce angle, as well as allowing you to change the direction of flight and disable physics simulation.

---
+++ Assets/Application/Scripts/Application/Scenes/Game/Units/Balls/BallComponents/MoveController.cs
@@ -0,0 +1,71 @@
+using Application.Scripts.Library.Extensions.Vector;
+using UnityEngine;
+
+namespace Application.Scripts.Application.Scenes.Game.Units.Balls.BallComponents
+{
+    public class MoveController : MonoBehaviour
+    {
+        private Vector2 _previousDirection;
+        private Vector2 _currentDirection;
+        
+        [SerializeField] private Rigidbody2D rigidbody2d;
+        [SerializeField] private float speed;
+        [SerializeField] private float minBounceAngel;
+        
+        public bool PhysicActive { get => rigidbody2d.isKinematic; set => rigidbody2d.isKinematic = !value; }
+
+        public void SetDirection(Vector2 direction)
+        {
+            direction = direction.normalized;
+            rigidbody2d.velocity = direction * speed;
+            _currentDirection = direction;
+        }
+        
+        private void FixedUpdate()
+        {
+            if (rigidbody2d.velocity.sqrMagnitude == 0f)
+            {
+                SetDirection(_previousDirection);
+            }
+            
+            rigidbody2d.velocity = rigidbody2d.velocity.normalized * speed;
+        }
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            _previousDirection = _currentDirection;
+            _currentDirection = rigidbody2d.velocity.normalized;
+
+            CheckAngel();
+        }
+
+        private void CheckAngel()
+        {
+            Vector2 horizontal = new Vector2((_currentDirection.x < 0) ? -1f : 1f, 0);
+            Vector2 vertical = new Vector2(0f, (_currentDirection.y < 0) ? -1f : 1f);
+
+            float horizontalAngel = Vector2.SignedAngle(_currentDirection, horizontal);
+            float verticalAngel = Vector2.SignedAngle(_currentDirection, vertical);
+
+            if (Mathf.Abs(horizontalAngel) < minBounceAngel)
+            {
+                CorrectAngel(horizontalAngel, horizontal);
+            }
+            else if (Mathf.Abs(verticalAngel) < minBounceAngel)
+            {
+                CorrectAngel(verticalAngel, vertical);
+            }
+        }
+
+        private void CorrectAngel(float angel, Vector2 side)
+        {
+            if (angel == 0)
+            {
+                angel = -Vector2.SignedAngle(side, _previousDirection);
+            }
+                
+            Vector2 newDirection = side.Rotate(minBounceAngel * (angel > 0 ? 1 : -1));
+            SetDirection(newDirection);
+        }
+    }
+}
