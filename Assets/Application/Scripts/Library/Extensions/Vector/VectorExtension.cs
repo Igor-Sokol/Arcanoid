ae99858270a6d22dd8067fd4afabf4a5be6c455e
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-04 20:27:39 +0300

    Added extensions for vectors

---
+++ Assets/Application/Scripts/Library/Extensions/Vector/VectorExtension.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Application.Scripts.Library.Extensions.Vector
+{
+    public static class VectorExtension
+    {
+        public static Vector3 Rotate(this Vector3 direction, float degree)
+        {
+            var cos = Mathf.Cos(-degree * Mathf.Deg2Rad);
+            var sin = Mathf.Sin(-degree * Mathf.Deg2Rad);
+            var rotatedVector = new Vector3(
+                direction.x * cos - direction.y * sin,
+                direction.x * sin + direction.y * cos
+            );
+
+            return rotatedVector;
+        }
+        
+        public static Vector2 Rotate(this Vector2 direction, float degree) => Rotate((Vector3)direction, degree);
+    }
+}
