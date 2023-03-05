43d195a0b9c333914f431d70a7e5beaebf90f6e3
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-05 14:37:41 +0300

    Added BallManager, through which will happen all interaction with the balls on the playing field, for example, adding / removing boosts

---
+++ Assets/Application/Scripts/Application/Scenes/Game/GameManagers/BallsManagers/BallsManager.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using Application.Scripts.Application.Scenes.Game.Pools.BallProviders.Contracts;
+using Application.Scripts.Application.Scenes.Game.Units.Balls;
+using Application.Scripts.Library.Reusable;
+using UnityEngine;
+
+namespace Application.Scripts.Application.Scenes.Game.GameManagers.BallsManagers
+{
+    public class BallsManager : MonoBehaviour, IReusable
+    {
+        private readonly List<Ball> _activeBalls = new List<Ball>();
+
+        [SerializeField] private BallProvider ballProvider;
+
+        public int BallsCount => _activeBalls.Count;
+        public IEnumerable<Ball> Balls => _activeBalls;
+
+        public Ball GetBall()
+        {
+            var ball = ballProvider.GetBall();
+            _activeBalls.Add(ball);
+            ball.PrepareReuse();
+            return ball;
+        }
+
+        public void ReturnBall(Ball ball)
+        {
+            _activeBalls.Remove(ball);
+            ballProvider.Return(ball);
+        }
+
+        public void PrepareReuse()
+        {
+            foreach (var ball in _activeBalls)
+            {
+                ballProvider.Return(ball);
+            }
+            _activeBalls.Clear();
+        }
+    }
+}
