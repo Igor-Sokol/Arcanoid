e03556d9aec8b2c2822ec86290920c7ee397eec1
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-05 14:34:39 +0300

    Deleted unused files

--- Assets/Application/Scripts/Application/Scenes/Game/Screen/PlayingField/BlockContainers/BlockContainer.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Application.Scripts.Application.Scenes.Game.Screen.PlayingField.BlockContainers
-{
-    public class BlockContainer : MonoBehaviour
-    {
-        
-    }
-}
