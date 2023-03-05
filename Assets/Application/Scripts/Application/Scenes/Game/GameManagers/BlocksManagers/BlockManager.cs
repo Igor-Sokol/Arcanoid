5bc2523c5ecb214a5e4363b123707b2dc6190ed9
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-05 14:40:01 +0300

    Added BlockManager, needed for the interaction of bonus blocks, as well as the implementation of the destruction of blocks will be transferred to the interaction with the manager, rather than directly with the provider

---
+++ Assets/Application/Scripts/Application/Scenes/Game/GameManagers/BlocksManagers/BlockManager.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+using Application.Scripts.Application.Scenes.Game.GameManagers.BlocksManagers.PackPlacers.Contracts;
+using Application.Scripts.Application.Scenes.Game.Pools.BlockProviders.Contracts;
+using Application.Scripts.Application.Scenes.Game.Units.Blocks;
+using Application.Scripts.Library.Reusable;
+using UnityEngine;
+
+namespace Application.Scripts.Application.Scenes.Game.GameManagers.BlocksManagers
+{
+    public class BlockManager : MonoBehaviour, IReusable
+    {
+        private Block[][] _blocks;
+        
+        [SerializeField] private BlockProvider blockProvider;
+        [SerializeField] private PackPlacer packPlacer;
+
+        public IEnumerable<Block> Blocks => _blocks.SelectMany(blocks => blocks);
+
+        public void PrepareReuse()
+        {
+            foreach (var block in Blocks)
+            {
+                block.PrepareReuse();
+            }
+        }
+        
+        public void SetBlocks(string[][] blockKeys)
+        {
+            _blocks = new Block[blockKeys.Length][];
+
+            for (int i = 0; i < blockKeys.Length; i++)
+            {
+                _blocks[i] = new Block[blockKeys[i].Length];
+                for (int j = 0; j < blockKeys[i].Length; j++)
+                {
+                    _blocks[i][j] = blockProvider.GetBlock(blockKeys[i][j]);
+                }
+            }
+            
+            packPlacer.Place(_blocks);
+        }
+    }
+}
