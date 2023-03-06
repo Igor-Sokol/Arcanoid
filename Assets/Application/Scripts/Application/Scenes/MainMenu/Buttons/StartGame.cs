6a8c681e81a29bd33acfe9cfde1b8ca78dc8549e
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-07 01:26:47 +0300

    Added script for the button on the main scene

---
+++ Assets/Application/Scripts/Application/Scenes/MainMenu/Buttons/StartGame.cs
@@ -0,0 +1,38 @@
+using System;
+using Application.Scripts.Application.Scenes.Shared.LibraryImplementations.SceneManagers.Loading;
+using Application.Scripts.Library.DependencyInjection;
+using Application.Scripts.Library.InitializeManager.Contracts;
+using Application.Scripts.Library.SceneManagers.Contracts.SceneManagers;
+using UnityEngine;
+using UnityEngine.UI;
+using Scene = Application.Scripts.Library.SceneManagers.Contracts.SceneInfo.Scene;
+
+namespace Application.Scripts.Application.Scenes.MainMenu.Buttons
+{
+    public class StartGame : MonoBehaviour, IInitializing
+    {
+        private ISceneManager _sceneManager;
+        
+        [SerializeField] private Button button;
+
+        public void Initialize()
+        {
+            _sceneManager = ProjectContext.Instance.GetService<ISceneManager>();
+        }
+        
+        private void OnEnable()
+        {
+            button.onClick.AddListener(LoadChoosePack);
+        }
+
+        private void OnDisable()
+        {
+            button.onClick.RemoveListener(LoadChoosePack);
+        }
+
+        private void LoadChoosePack()
+        {
+            _sceneManager.LoadScene<DefaultSceneLoading>(Scene.ChoosePack);
+        }
+    }
+}
