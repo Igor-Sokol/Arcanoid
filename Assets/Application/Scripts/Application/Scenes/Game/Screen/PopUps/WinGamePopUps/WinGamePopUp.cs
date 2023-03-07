0cd5752c8cb2b65cbb926a6a3271a7f45a92b21d
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-08 00:53:33 +0300

    Added win popup

---
+++ Assets/Application/Scripts/Application/Scenes/Game/Screen/PopUps/WinGamePopUps/WinGamePopUp.cs
@@ -0,0 +1,69 @@
+using System;
+using Application.Scripts.Application.Scenes.Shared.ProgressManagers.PackProgress.PacksInfo.Contracts;
+using Application.Scripts.Library.PopUpManagers.AnimationContracts;
+using Application.Scripts.Library.PopUpManagers.PopUpContracts;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Application.Scripts.Application.Scenes.Game.Screen.PopUps.WinGamePopUps
+{
+    public class WinGamePopUp : PopUp
+    {
+        [SerializeField] private Button continueButton;
+        [SerializeField] private Button menuButton;
+        [SerializeField] private WinGamePopUpView popUpView;
+        
+        public override bool Active => gameObject.activeSelf;
+        public Button ContinueButton => continueButton;
+        public Button MenuButton => menuButton;
+        public override IPopUpAnimator PopUpAnimator { get; set; }
+        public override event Action OnShown;
+        public override event Action OnHidden;
+        public event Action OnContinueSelected;
+        public event Action OnMenuSelected;
+
+        public void Configure(IPackInfo packInfo)
+        {
+            popUpView.Configure(packInfo);
+        }
+        
+        public override void Show()
+        {
+            gameObject.SetActive(true);
+            OnShown?.Invoke();
+        }
+
+        public override void Hide()
+        {
+            gameObject.SetActive(false);
+            OnHidden?.Invoke();
+            
+            OnHidden = null;
+            OnShown = null;
+            OnContinueSelected = null;
+            OnMenuSelected = null;
+        }
+
+        private void OnEnable()
+        {
+            continueButton.onClick.AddListener(ContinueSelected);
+            menuButton.onClick.AddListener(OnMenu);
+        }
+
+        private void OnDisable()
+        {
+            continueButton.onClick.RemoveListener(ContinueSelected);
+            menuButton.onClick.RemoveListener(OnMenu);
+        }
+        
+        private void ContinueSelected()
+        {
+            OnContinueSelected?.Invoke();
+        }
+        
+        private void OnMenu()
+        {
+            OnMenuSelected?.Invoke();
+        }
+    }
+}
---
+++ Assets/Application/Scripts/Application/Scenes/Game/Screen/PopUps/WinGamePopUps/WinGamePopUpView.cs
@@ -0,0 +1,28 @@
+using Application.Scripts.Application.Scenes.Shared.ProgressManagers.PackProgress.PacksInfo.Contracts;
+using Application.Scripts.Application.Scenes.Shared.ProgressView;
+using Application.Scripts.Library.DependencyInjection;
+using Application.Scripts.Library.Localization.LocalizationManagers;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Application.Scripts.Application.Scenes.Game.Screen.PopUps.WinGamePopUps
+{
+    public class WinGamePopUpView : MonoBehaviour
+    {
+        private ILocalizationManager _localizationManager;
+        
+        [SerializeField] private Image packImage;
+        [SerializeField] private TMP_Text packName;
+        [SerializeField] private ProgressView packProgress;
+
+        public void Configure(IPackInfo packInfo)
+        {
+            _localizationManager ??= ProjectContext.Instance.GetService<ILocalizationManager>();
+            
+            packImage.sprite = packInfo.LevelPack.PackImage;
+            packName.text = _localizationManager.GetString(packInfo.LevelPack.PackNameKey);
+            packProgress.SetProgress(packInfo.CurrentLevelIndex + 1, packInfo.LevelPack.LevelCount);
+        }
+    }
+}
