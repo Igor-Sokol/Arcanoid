0aae23124dfb4b36024c3bd5e4aa761f67a0190d
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-08 00:46:35 +0300

    Gameplay manager has been simplified and is now directly responsible for launching the level

--- Assets/Application/Scripts/Application/Scenes/Game/GameManagers/GameplayManagers/GameplayManager.cs
+++ Assets/Application/Scripts/Application/Scenes/Game/GameManagers/GameplayManagers/GameplayManager.cs
@@ -1,6 +1,7 @@
 using Application.Scripts.Application.Scenes.Game.GameManagers.BallsManagers;
 using Application.Scripts.Application.Scenes.Game.GameManagers.BlocksManagers;
 using Application.Scripts.Application.Scenes.Game.GameManagers.HealthManagers;
+using Application.Scripts.Application.Scenes.Game.GameManagers.ProcessManagers;
 using Application.Scripts.Application.Scenes.Game.Units.Platform;
 using Application.Scripts.Application.Scenes.Shared.LevelManagement.Levels;
 using UnityEngine;
@@ -11,48 +12,19 @@ namespace Application.Scripts.Application.Scenes.Game.GameManagers.GameplayManag
     {
         [SerializeField] private HealthManager healthManager;
         [SerializeField] private BlockManager blockManager;
+        [SerializeField] private BlockProgressManager blockProgressManager;
         [SerializeField] private BallsManager ballsManager;
         [SerializeField] private Platform platform;
 
-        private void OnEnable()
-        {
-            ballsManager.OnAllBallRemoved += OnMissedBalls;
-            healthManager.OnDead += OnPlayerLose;
-        }
-
-        private void OnDisable()
-        {
-            ballsManager.OnAllBallRemoved -= OnMissedBalls;
-            healthManager.OnDead -= OnPlayerLose;
-        }
-
         public void StartGame(LevelInfo levelInfo)
         {
+            healthManager.PrepareReuse();
             blockManager.PrepareReuse();
             ballsManager.PrepareReuse();
 
             blockManager.SetBlocks(levelInfo.LevelReader.ReadPack(levelInfo));
+            blockProgressManager.PrepareReuse();
             platform.BallLauncher.SetBall(ballsManager.GetBall());
-        }
-
-        private void OnMissedBalls()
-        {
-            healthManager.RemoveHealth();
-
-            if (healthManager.CurrentHealth > 0)
-            {
-                platform.BallLauncher.SetBall(ballsManager.GetBall());
-            }
-        }
-
-        private void OnPlayerLose()
-        {
-            
-        }
-
-        private void OnPlayerWin()
-        {
-            
         }
     }
 }
