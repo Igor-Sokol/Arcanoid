f06c4e097faf4506f8a313c60d1709623507f2bc
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-07 13:16:33 +0300

    Added HealthViewManager

---
+++ Assets/Application/Scripts/Application/Scenes/Game/Screen/UI/Health/HealthViewManagers/HealthViewManager.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Application.Scripts.Application.Scenes.Game.GameManagers.HealthManager;
+using Application.Scripts.Application.Scenes.Game.Screen.UI.Health.HealthViews;
+using Application.Scripts.Library.InitializeManager.Contracts;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Application.Scripts.Application.Scenes.Game.Screen.UI.Health.HealthViewManagers
+{
+    public class HealthViewManager : MonoBehaviour, IInitializing
+    {
+        private readonly List<HealthView> _healthViews = new List<HealthView>();
+        private Vector2 _viewImageSize;
+
+        [SerializeField] private HealthView healthViewPrefab;
+        [SerializeField] private HealthManager healthManager;
+        [SerializeField] private RectTransform rectTransform;
+        [SerializeField] private GridLayoutGroup gridLayoutGroup;
+        [SerializeField] private Transform container;
+
+        public void Initialize()
+        {
+            SetViewScale(healthManager.MaxHealth);
+            GenerateViews(healthManager.MaxHealth);
+            InitViews(healthManager.CurrentHealth);
+        }
+
+        private void OnEnable()
+        {
+            healthManager.OnHealthAdded += OnHealthAdded;
+            healthManager.OnHealthRemoved += OnHealthRemoved;
+        }
+
+        private void OnDisable()
+        {
+            healthManager.OnHealthAdded -= OnHealthAdded;
+            healthManager.OnHealthRemoved -= OnHealthRemoved;
+        }
+
+        private void InitViews(int currentHealth)
+        {
+            for (int i = 0; i < currentHealth; i++)
+            {
+                OnHealthAdded();
+            }
+        }
+        
+        private void GenerateViews(int viewCount)
+        {
+            for (int i = 0; i < viewCount; i++)
+            {
+                var view = Instantiate(healthViewPrefab, container);
+                _healthViews.Add(view);
+            }
+        }
+
+        private void SetViewScale(int viewCount)
+        {
+            _viewImageSize = healthViewPrefab.Sprite.rect.size;
+            
+            int weigh = Mathf.Clamp(viewCount, 0, gridLayoutGroup.constraintCount);
+            int height = Mathf.CeilToInt(viewCount / (float)gridLayoutGroup.constraintCount);
+            
+            Vector2 contentSpace;
+            contentSpace.x = weigh * gridLayoutGroup.spacing.x;
+            contentSpace.y = height * Mathf.CeilToInt(viewCount / (float)gridLayoutGroup.constraintCount) * gridLayoutGroup.spacing.x;
+
+            Vector2 spaceSize = rectTransform.rect.size - contentSpace;
+            spaceSize /= new Vector2(weigh, height);
+
+            Vector2 scales = new Vector2(spaceSize.x / _viewImageSize.x, spaceSize.y / _viewImageSize.y);
+
+            float scale = scales.x < scales.y ? scales.x : scales.y;
+
+            gridLayoutGroup.cellSize = new Vector2(_viewImageSize.x * scale, _viewImageSize.y * scale);
+        }
+
+        private void OnHealthAdded()
+        {
+            _healthViews.LastOrDefault(h => !h.Enabled)?.Enable();
+        }
+        
+        private void OnHealthRemoved()
+        {
+            _healthViews.FirstOrDefault(h => h.Enabled)?.Disable();
+        }
+    }
+}
