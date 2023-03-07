3c8c4e0c37010b571addafa1884b4984338b5fb6
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-08 00:49:26 +0300

    Added implementation of an exit button from the level pack selection stage

---
+++ Assets/Application/Scripts/Application/Scenes/ChoosePack/UI/BackButton.cs
@@ -0,0 +1,29 @@
+using Application.Scripts.Application.Scenes.Shared.LibraryImplementations.SceneManagers.Loading;
+using Application.Scripts.Library.DependencyInjection;
+using Application.Scripts.Library.SceneManagers.Contracts.SceneInfo;
+using Application.Scripts.Library.SceneManagers.Contracts.SceneManagers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Application.Scripts.Application.Scenes.ChoosePack.UI
+{
+    public class BackButton : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+
+        private void OnEnable()
+        {
+            button.onClick.AddListener(OnClick);
+        }
+
+        private void OnDisable()
+        {
+            button.onClick.RemoveListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            ProjectContext.Instance.GetService<ISceneManager>().LoadScene<DefaultSceneLoading>(Scene.MainMenu);
+        }
+    }
+}
