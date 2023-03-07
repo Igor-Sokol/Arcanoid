90c939d0961b2481929ee04a7db251f9ef827346
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-08 00:50:01 +0300

    Added BlockPregress manager, which is responsible for the current number of broken blocks in the scene

---
+++ Assets/Application/Scripts/Application/Scenes/Game/GameManagers/ProcessManagers/BlockProgressManager.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Linq;
+using Application.Scripts.Application.Scenes.Game.GameManagers.BlocksManagers;
+using Application.Scripts.Application.Scenes.Game.Screen.UI.LevelPackViews;
+using Application.Scripts.Application.Scenes.Game.Units.Blocks;
+using Application.Scripts.Application.Scenes.Game.Units.Blocks.BlockComponents.HitServices.Implementations;
+using Application.Scripts.Library.Reusable;
+using UnityEngine;
+
+namespace Application.Scripts.Application.Scenes.Game.GameManagers.ProcessManagers
+{
+    public class BlockProgressManager : MonoBehaviour, IReusable
+    {
+        private int _brokenBlocks;
+        private int _totalBlocks;
+        
+        [SerializeField] private BlockManager blockManager;
+        [SerializeField] private LevelPackView levelPackView;
+
+        public int BrokenBlocks => _brokenBlocks;
+        public int TotalBlocks => _totalBlocks;
+        public event Action OnAllBlockBroken;
+
+        private void OnEnable()
+        {
+            blockManager.OnBlockRemoved += UpdateProgress;
+        }
+        private void OnDisable()
+        {
+            blockManager.OnBlockRemoved -= UpdateProgress;
+        }
+
+        public void PrepareReuse()
+        {
+            _totalBlocks =
+                blockManager.Blocks.Count(b => b.HitServiceManager.Services.OfType<BlockHealthService>().Any());
+            _brokenBlocks = 0;
+            
+            levelPackView.LevelProgress.SetProgress(_brokenBlocks / _totalBlocks * 100, 100);
+        }
+
+        private void UpdateProgress(Block block)
+        {
+            if (block.HitServiceManager.Services.OfType<BlockHealthService>().Any())
+            {
+                _brokenBlocks++;
+                levelPackView.LevelProgress.SetProgress((int)(_brokenBlocks / (float)_totalBlocks * 100f), 100);
+
+                if (_brokenBlocks == _totalBlocks)
+                {
+                    OnAllBlockBroken?.Invoke();
+                }
+            }
+        }
+    }
+}
