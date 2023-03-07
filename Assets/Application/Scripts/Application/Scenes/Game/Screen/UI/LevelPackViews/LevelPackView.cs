b2aaad6f705fc4ab3ba8fb0f6594cb8c3cacd25d
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-07 14:31:06 +0300

    Added LevelPackView for the game scene

---
+++ Assets/Application/Scripts/Application/Scenes/Game/Screen/UI/LevelPackViews/LevelPackView.cs
@@ -0,0 +1,17 @@
+using Application.Scripts.Application.Scenes.ChoosePack.LevelPacks.Components;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Application.Scripts.Application.Scenes.Game.Screen.UI.LevelPackViews
+{
+    public class LevelPackView : MonoBehaviour
+    {
+        [SerializeField] private ProgressView packProgress;
+        [SerializeField] private ProgressView levelProgress;
+        [SerializeField] private Image packImage;
+
+        public ProgressView PackProgress => packProgress;
+        public ProgressView LevelProgress => levelProgress;
+        public Sprite PackImage { get => packImage.sprite; set => packImage.sprite = value; }
+    }
+}
