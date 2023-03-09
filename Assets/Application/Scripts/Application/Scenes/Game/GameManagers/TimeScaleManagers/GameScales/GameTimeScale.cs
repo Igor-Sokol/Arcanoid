96d20cfcaa291e92c796111c7eb111a84df621e5
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-09 22:47:09 +0300

    GameTimeScale is now supported for GameActionManager

--- Assets/Application/Scripts/Application/Scenes/Game/GameManagers/TimeScaleManagers/GameScales/GameTimeScale.cs
+++ Assets/Application/Scripts/Application/Scenes/Game/GameManagers/TimeScaleManagers/GameScales/GameTimeScale.cs
@@ -1,13 +1,15 @@
+using Application.Scripts.Library.GameActionManagers.Contracts;
 using Application.Scripts.Library.InitializeManager.Contracts;
 using Application.Scripts.Library.TimeManagers.Contracts;
 using UnityEngine;
 
 namespace Application.Scripts.Application.Scenes.Game.GameManagers.TimeScaleManagers.GameScales
 {
-    public class GameTimeScale : TimeScaler, IInitializing
+    public class GameTimeScale : TimeScaler, IInitializing, IActionTimeScale
     {
         [SerializeField] private float startScale;
         public override float Scale { get; set; }
+        public float TimeScale => Scale;
         public void Initialize()
         {
             Scale = startScale;
