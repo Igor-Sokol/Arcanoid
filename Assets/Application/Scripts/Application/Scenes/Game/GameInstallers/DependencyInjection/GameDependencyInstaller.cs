baebf18322552af45665c792b00087a3798b96f0
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-12 01:42:42 +0300

    Registration of new gaming dependencies

--- Assets/Application/Scripts/Application/Scenes/Game/GameInstallers/DependencyInjection/GameDependencyInstaller.cs
+++ Assets/Application/Scripts/Application/Scenes/Game/GameInstallers/DependencyInjection/GameDependencyInstaller.cs
@@ -1,6 +1,12 @@
 using Application.Scripts.Application.Scenes.Game.GameManagers.BlocksManagers;
+using Application.Scripts.Application.Scenes.Game.GameManagers.BoostManagers;
+using Application.Scripts.Application.Scenes.Game.GameManagers.BoostManagers.Contracts;
+using Application.Scripts.Application.Scenes.Game.GameManagers.BoostObjectManagers;
+using Application.Scripts.Application.Scenes.Game.GameManagers.HealthManagers;
 using Application.Scripts.Library.DependencyInjection;
 using Application.Scripts.Library.InitializeManager.Contracts;
+using Application.Scripts.Library.TimeManagers;
+using Application.Scripts.Library.TimeManagers.Contracts;
 using UnityEngine;
 
 namespace Application.Scripts.Application.Scenes.Game.GameInstallers.DependencyInjection
@@ -8,10 +14,18 @@ namespace Application.Scripts.Application.Scenes.Game.GameInstallers.DependencyI
     public class GameDependencyInstaller : MonoBehaviour, IInitializing
     {
         [SerializeField] private BlockManager blockManager;
+        [SerializeField] private BoostObjectManager boostObjectManager;
+        [SerializeField] private BoostManager boostManager;
+        [SerializeField] private HealthManager healthManager;
+        [SerializeField] private TimeScaleManager timeScaleManager;
         
         public void Initialize()
         {
             ProjectContext.Instance.SetService<BlockManager, BlockManager>(blockManager);
+            ProjectContext.Instance.SetService<IBoostObjectManager, BoostObjectManager>(boostObjectManager);
+            ProjectContext.Instance.SetService<IBoostManager, BoostManager>(boostManager);
+            ProjectContext.Instance.SetService<IHealthManager, HealthManager>(healthManager);
+            ProjectContext.Instance.SetService<ITimeScaleManager, TimeScaleManager>(timeScaleManager);
         }
     }
 }
