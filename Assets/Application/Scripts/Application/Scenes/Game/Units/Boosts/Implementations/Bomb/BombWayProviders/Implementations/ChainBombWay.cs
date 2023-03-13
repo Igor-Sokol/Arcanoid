dd79d7ca2757d0c7608a981613d8d759a39da68a
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-14 01:14:18 +0300

    Added implementation for the chain bomb

---
+++ Assets/Application/Scripts/Application/Scenes/Game/Units/Boosts/Implementations/Bomb/BombWayProviders/Implementations/ChainBombWay.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+using Application.Scripts.Application.Scenes.Game.Units.Blocks;
+using Application.Scripts.Application.Scenes.Game.Units.Boosts.Implementations.Bomb.BombWayProviders.Contracts;
+using Application.Scripts.Library.ChainFinders;
+using UnityEngine;
+
+namespace Application.Scripts.Application.Scenes.Game.Units.Boosts.Implementations.Bomb.BombWayProviders.Implementations
+{
+    public class ChainBombWay : BombWay
+    {
+        [SerializeField] private Vector2[] moveRules;
+        
+        public override IEnumerable<IEnumerable<Vector2>> GetIndexes(Block[][] blocks, Vector2 startIndex, IEnumerable<string> ignoreKeys)
+        {
+            yield return ChainFinder.GetLongestChain(blocks, (b1, b2) =>
+                {
+                    if (b1 && b2)
+                    {
+                        return b1.Key == b2.Key && !ignoreKeys.Contains(b2.Key);
+                    }
+                    return false;
+                }, (b) => b,
+                startIndex, moveRules).Skip(1);
+        }
+    }
+}
