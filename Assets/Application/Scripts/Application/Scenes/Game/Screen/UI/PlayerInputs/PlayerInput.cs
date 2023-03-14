afadebd6cd1b5fae045298960379383125943d63
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-14 17:48:59 +0300

    Fixed platform control for the situation when the player touches the screen with more than one finger

--- Assets/Application/Scripts/Application/Scenes/Game/Screen/UI/PlayerInputs/PlayerInput.cs
+++ Assets/Application/Scripts/Application/Scenes/Game/Screen/UI/PlayerInputs/PlayerInput.cs
@@ -6,6 +6,8 @@ namespace Application.Scripts.Application.Scenes.Game.Screen.UI.PlayerInputs
 {
     public class PlayerInput : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
     {
+        private int _touchCount;
+        
         public event Action<Vector2> OnTouching;
         public event Action<Vector2> OnStartTouching;
         public event Action<Vector2> OnStopTouching;
@@ -17,12 +19,18 @@ namespace Application.Scripts.Application.Scenes.Game.Screen.UI.PlayerInputs
 
         public void OnPointerDown(PointerEventData eventData)
         {
+            _touchCount++;
             OnStartTouching?.Invoke(eventData.pointerCurrentRaycast.worldPosition);
         }
 
         public void OnPointerUp(PointerEventData eventData)
         {
-            OnStopTouching?.Invoke(eventData.pointerCurrentRaycast.worldPosition);
+            _touchCount--;
+
+            if (_touchCount <= 0)
+            {
+                OnStopTouching?.Invoke(eventData.pointerCurrentRaycast.worldPosition);
+            }
         }
     }
 }
