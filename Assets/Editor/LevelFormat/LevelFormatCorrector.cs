04f9227c2992e04a2a4906f38cb2f0a8675e3758
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-15 23:00:17 +0300

    Added Level Format Corrector, which automatically corrects the Tiled export format to the format required for the game

---
+++ Assets/Editor/LevelFormat/LevelFormatCorrector.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.LevelFormat
+{
+    public class LevelFormatCorrector : MonoBehaviour
+    {
+        [MenuItem("Assets/Fix Level Format")]
+        private static void CorrectLevelFormat()
+        {
+            var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+
+            try
+            {
+                var transferObject = JsonConvert.DeserializeObject<LevelTransferObject>(File.ReadAllText(path));
+                string newFormat = JsonConvert.SerializeObject(transferObject.LevelInfos[0]);
+                File.WriteAllText(path, newFormat);
+                AssetDatabase.Refresh();
+            }
+            catch (Exception)
+            {
+                Debug.LogWarning("The file does not match the Tiled format.");
+            }
+        }
+        
+        [MenuItem("Assets/Fix Level Format", true)]
+        private static bool CorrectLevelFormatValidation()
+        {
+            return Selection.activeObject.GetType() == typeof(TextAsset);
+        }
+        
+        private struct LevelTransferObject
+        {
+            [JsonProperty("layers")]
+            public LevelInfo[] LevelInfos;
+        }
+
+        private struct LevelInfo
+        {
+            public int[] Data;
+            public int Height;
+            public int Width;
+        }
+    }
+}
