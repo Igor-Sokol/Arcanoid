a7d9d3c9596691f4c94e89a08585d6d287d9bd3c
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-15 17:47:35 +0300

    Level refactoring

--- Assets/Application/Scripts/Application/Scenes/Shared/LevelManagement/PackManager.cs
+++ Assets/Application/Scripts/Application/Scenes/Shared/LevelManagement/PackManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace Application.Scripts.Application.Scenes.Shared.LevelManagement
 {
-    [CreateAssetMenu(fileName = "LevelPack", menuName = "LevelManagement/PackManager")]
+    [CreateAssetMenu(fileName = "PackManager", menuName = "LevelManagement/PackManager")]
     public class PackManager : ScriptableObject
     {
         [SerializeField] private LevelPack[] levelPacks;
