d1562e78672f4f03a59bbb853fe489ca131e047f
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-16 02:27:26 +0300

    Added components to enable and disable ball collision

--- Assets/Application/Scripts/Application/Scenes/Game/Units/Balls/Ball.cs
+++ Assets/Application/Scripts/Application/Scenes/Game/Units/Balls/Ball.cs
@@ -11,16 +11,23 @@ namespace Application.Scripts.Application.Scenes.Game.Units.Balls
         [SerializeField] private MoveController moveController;
         [SerializeField] private TimeManager timeManager;
         [SerializeField] private BallHitManager ballHitManager;
+        [SerializeField] private CircleCollider2D collisionCollider;
+        [SerializeField] private CircleCollider2D triggerCollider;
 
         public MoveController MoveController => moveController;
         public TimeManager TimeManager => timeManager;
         public BallHitManager BallHitManager => ballHitManager;
+        public bool EnableCollision { get => collisionCollider.enabled; set => collisionCollider.enabled = value; }
+        public bool EnableTrigger { get => triggerCollider.enabled; set => triggerCollider.enabled = value; }
+        public float Radius => collisionCollider.radius;
 
         public void PrepareReuse()
         {
             moveController.PrepareReuse();
             TimeManager.ClearTimeScales();
             BallHitManager.PrepareReuse();
+            EnableCollision = true;
+            EnableTrigger = true;
         }
     }
 }
