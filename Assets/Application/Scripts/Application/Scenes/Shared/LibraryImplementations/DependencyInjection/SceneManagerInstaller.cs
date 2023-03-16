56b1bdcf64354ecc35437748259dd32d9cfdee73
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-17 02:28:38 +0300

    Added animated transition between scenes

--- Assets/Application/Scripts/Application/Scenes/Shared/LibraryImplementations/DependencyInjection/SceneManagerInstaller.cs
+++ Assets/Application/Scripts/Application/Scenes/Shared/LibraryImplementations/DependencyInjection/SceneManagerInstaller.cs
@@ -1,5 +1,6 @@
 using Application.Scripts.Library.DependencyInjection;
 using Application.Scripts.Library.DependencyInjection.Contracts;
+using Application.Scripts.Library.InitializeManager;
 using Application.Scripts.Library.SceneManagers;
 using Application.Scripts.Library.SceneManagers.Contracts.SceneManagers;
 using UnityEngine;
@@ -11,11 +12,13 @@ namespace Application.Scripts.Application.Scenes.Shared.LibraryImplementations.D
         private ProjectContext _projectContext;
 
         [SerializeField] private SceneManager sceneManager;
+        [SerializeField] private Initializer loadingUIInitializer;
         
         public override ProjectContext ProjectContext { get => _projectContext ??= ProjectContext.Instance; set => _projectContext = value; }
         public override void InstallService()
         {
             ProjectContext.Instance.SetService<ISceneManager, SceneManager>(sceneManager);
+            loadingUIInitializer.Initialize();
         }
     }
 }
--- Assets/Application/Scripts/Application/Scenes/Shared/LibraryImplementations/SceneManagers/Loading/DefaultSceneLoading.cs
+++ Assets/Application/Scripts/Application/Scenes/Shared/LibraryImplementations/SceneManagers/Loading/DefaultSceneLoading.cs
@@ -1,22 +1,101 @@
+using System.Collections.Generic;
+using Application.Scripts.Library.InitializeManager.Contracts;
 using Application.Scripts.Library.SceneManagers.Contracts.Loading;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Application.Scripts.Application.Scenes.Shared.LibraryImplementations.SceneManagers.Loading
 {
-    public class DefaultSceneLoading : SceneLoading
+    public class DefaultSceneLoading : SceneLoading, IInitializing
     {
+        private readonly List<Image> _images = new List<Image>();
+
+        private Sequence _animation;
+
         [SerializeField] private Canvas canvas;
+        [SerializeField] private GridLayoutGroup gridLayoutGroup;
+        [SerializeField] private Image prefab;
+        [SerializeField] private Sprite[] blocks;
+        [SerializeField] private float animationTime;
+        [SerializeField] private Vector2 defaultImageSize;
+        
+        public void Initialize()
+        {
+            float scale = (Screen.width / (float)gridLayoutGroup.constraintCount) / defaultImageSize.x;
+
+            Vector2 imageSize = defaultImageSize * scale;
+            gridLayoutGroup.cellSize = defaultImageSize * scale;
+            
+            var imageCount = new Vector2Int(gridLayoutGroup.constraintCount, Mathf.CeilToInt(Screen.height / imageSize.y));
+
+            int count = imageCount.x * imageCount.y;
+            for (int i = 0; i < count; i++)
+            {
+                var instance = Instantiate(prefab, gridLayoutGroup.transform);
+                instance.color = Color.clear;
+                instance.sprite = blocks[i % blocks.Length];
+                _images.Add(instance);
+            }
+        }
         
         public override YieldInstruction Enable()
         {
             canvas.gameObject.SetActive(true);
-            return null;
+            Show();
+            return new WaitForSeconds(animationTime);
         }
 
         public override YieldInstruction Disable()
         {
-            canvas.gameObject.SetActive(false);
-            return null;
+            Hide();
+            return new WaitForSeconds(animationTime);
+        }
+
+        private void Show()
+        {
+            gridLayoutGroup.enabled = true;
+            for (int i = 0; i < _images.Count; i++)
+            {
+                int index = Random.Range(i, _images.Count);
+                (_images[i], _images[index]) = (_images[index], _images[i]);
+                _images[i].transform.SetSiblingIndex(i);
+            }
+            
+            _animation?.Kill();
+            _animation = DOTween.Sequence();
+
+            float itemDuration = animationTime / _images.Count;
+
+            foreach (var image in _images)
+            {
+                var color = Color.white;
+                color.a = 0f;
+                image.color = color;
+                
+                _animation.Append(image.DOFade(1, itemDuration));
+            }
+        }
+
+        private void Hide()
+        {
+            gridLayoutGroup.enabled = false;
+            
+            _animation?.Kill();
+            _animation = DOTween.Sequence();
+
+            float itemDuration = animationTime / _images.Count;
+            
+            foreach (var image in _images)
+            {
+                var color = Color.white;
+                color.a = 1f;
+                image.color = color;
+                
+                _animation.Append(image.DOFade(0, itemDuration));
+            }
+            
+            _animation.OnComplete(() => canvas.gameObject.SetActive(false));
         }
     }
 }
