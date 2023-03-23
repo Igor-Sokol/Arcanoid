58789e8fc6525082a902b8c4afb00815f7b4d5a1
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-24 00:09:01 +0300

    Background path animation now depends on the pause

--- Assets/Application/Scripts/Application/Scenes/Game/Screen/UI/Background/LineAnimator.cs
+++ Assets/Application/Scripts/Application/Scenes/Game/Screen/UI/Background/LineAnimator.cs
@@ -1,17 +1,34 @@
+using Application.Scripts.Application.Scenes.Shared.DoTweenGameActions;
+using Application.Scripts.Application.Scenes.Shared.LibraryImplementations.TimeManagers;
+using Application.Scripts.Library.DependencyInjection;
+using Application.Scripts.Library.GameActionManagers.Contracts;
+using Application.Scripts.Library.GameActionManagers.Timer;
+using Application.Scripts.Library.InitializeManager.Contracts;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Application.Scripts.Application.Scenes.Game.Screen.UI.Background
 {
-    public class LineAnimator : MonoBehaviour
+    public class LineAnimator : MonoBehaviour, IInitializing
     {
+        private IGameActionManager _gameActionManager;
         private Sequence _animation;
+        private ActionHandler _animationHandler;
     
         [SerializeField] private Image image;
+        [SerializeField] private ActionTimeManager actionTimeManager;
         [SerializeField] private float stepDuration;
+        
+        public void Initialize()
+        {
+            _gameActionManager = ProjectContext.Instance.GetService<IGameActionManager>();
+        }
+        
         private void Play()
         {
+            _animationHandler.Stop();
+            
             _animation?.Kill();
             _animation = DOTween.Sequence();
 
@@ -21,14 +38,21 @@ namespace Application.Scripts.Application.Scenes.Game.Screen.UI.Background
             _animation.Append(image.DOFillAmount(0, stepDuration).SetEase(Ease.Linear));
             _animation.AppendCallback(() => image.fillOrigin = 1);
             _animation.SetLoops(-1);
+            
+            _animationHandler = _gameActionManager.StartAction(new DoTweenGameAction(_animation), -1, actionTimeManager);
         }
 
         private void Stop()
         {
+            _animationHandler.Stop();
             _animation?.Kill();
         }
 
-        private void OnEnable() => Play();
+        private void OnEnable()
+        {
+            Initialize();
+            Play();
+        }
 
         private void OnDisable() => Stop();
     }
