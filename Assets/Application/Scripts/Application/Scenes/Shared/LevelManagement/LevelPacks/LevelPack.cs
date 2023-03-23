0923c6ea6462a5a22f70ade26b1e1669a34cf800
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-23 22:55:37 +0300

    Added ability to automatically add levels from the folder to the pack

--- Assets/Application/Scripts/Application/Scenes/Shared/LevelManagement/LevelPacks/LevelPack.cs
+++ Assets/Application/Scripts/Application/Scenes/Shared/LevelManagement/LevelPacks/LevelPack.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Application.Scripts.Application.Scenes.Shared.LevelManagement.Levels;
 using Application.Scripts.Application.Scenes.Shared.LevelManagement.Levels.Readers.Contracts;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Application.Scripts.Application.Scenes.Shared.LevelManagement.LevelPacks
@@ -12,13 +15,27 @@ namespace Application.Scripts.Application.Scenes.Shared.LevelManagement.LevelPac
         [SerializeField] private Sprite packImage;
         [SerializeField] private LevelReader levelReader;
         [SerializeField] private string packPath;
-        [SerializeField] private string[] levelNames;
+        [SerializeField] private List<string> levelNames;
 
         public string PackNameKey => packNameKey;
         public Sprite PackImage => packImage;
         public LevelInfo[] Levels => ReadLevels();
-        public int LevelCount => levelNames.Length;
+        public int LevelCount => levelNames.Count;
 
+        [Conditional("UNITY_EDITOR"), Button("Load Levels")]
+        private void LoadLevels()
+        {
+            var assets = Resources.LoadAll<TextAsset>(packPath);
+            foreach (var asset in assets)
+            {
+                if (!levelNames.Contains(asset.name))
+                {
+                    levelNames.Add(asset.name);
+                }
+                Resources.UnloadAsset(asset);
+            }
+        }
+        
         private LevelInfo[] ReadLevels()
         {
             return levelNames.Select(p => new LevelInfo(packPath + p, levelReader)).ToArray();
