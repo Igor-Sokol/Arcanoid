5d60e02891ebecec4e6d5d6ff90a546f265be8f2
Igor Sokol <igor.sokol.isa@gmail.com>
2023-03-29 20:59:05 +0300

    Added effect of the ball going out of the screen boundaries

--- Assets/Application/Scripts/Application/Scenes/Game/Screen/PlayingField/Borders/BallDeadZone.cs
+++ Assets/Application/Scripts/Application/Scenes/Game/Screen/PlayingField/Borders/BallDeadZone.cs
@@ -1,5 +1,6 @@
 using Application.Scripts.Application.Scenes.Game.GameManagers.BallsManagers;
 using Application.Scripts.Application.Scenes.Game.Units.Balls;
+using Application.Scripts.Library.ScreenInfo;
 using UnityEngine;
 
 namespace Application.Scripts.Application.Scenes.Game.Screen.PlayingField.Borders
@@ -7,11 +8,18 @@ namespace Application.Scripts.Application.Scenes.Game.Screen.PlayingField.Border
     public class BallDeadZone : MonoBehaviour
     {
         [SerializeField] private BallsManager ballsManager;
+        [SerializeField] private ScreenInfo screenInfo;
+        [SerializeField] private ParticleSystem deadParticles;
+        [SerializeField] private Vector2 screenPosition;
 
         private void OnCollisionEnter2D(Collision2D col)
         {
             if (col.collider.TryGetComponent<Ball>(out var ball))
             {
+                Instantiate(deadParticles,
+                    new Vector3(ball.transform.position.x, screenInfo.PositionFromPercentage(screenPosition).y),
+                    Quaternion.identity);
+                
                 ballsManager.ReturnBall(ball);
             }
         }
